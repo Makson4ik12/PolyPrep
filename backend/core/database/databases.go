f5834741a4c6f89559646fa05115093b5b538534
Koza <ser9ev@gmail.com>
2025-04-25 17:46:16 +0300

    add to migration

--- backend/core/database/databases.go
+++ backend/core/database/databases.go
@@ -18,7 +18,7 @@ func ConnectDB() {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
-	db.AutoMigrate(&models.Post{}, &models.Include{}, &models.Like{}, &models.Comment{}, &models.User{})
+	db.AutoMigrate(&models.Post{}, &models.Include{}, &models.Like{}, &models.Comment{}, &models.User{}, &models.Favourite{})
 
 	DB = db
 }
